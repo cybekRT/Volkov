a369fd1a427fff1b05ce706f1540429fec5c140c
cybek <cybek@wyobraz.net>
2019-12-25 00:05:26 +0100

    Initial Commit

---
+++ Volkov/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Volkov
+//
+//  Created by cybek on 25/12/2019.
+//  Copyright © 2019 cybek. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
